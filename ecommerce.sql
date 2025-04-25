54ef39cc33e363dbc844c070cef6a5870020f9d5
kholofeloj <kholofelojmocheko@gmail.com>
2025-04-25 17:13:03 +0200

    added size_option, product_category, brand and color tables

--- ecommerce.sql
+++ ecommerce.sql
@@ -17,3 +17,29 @@ CREATE TABLE size_category (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(150) NOT NULL
 );
+
+-- size option table
+CREATE TABLE size_option (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VAECHAR(150) NOT NULL,
+    size_category_id INT NOT NULL,
+    FOREIGN KEY (size_category_id) REFERENCES size_category(id)
+);
+
+-- product category table
+CREATE TABLE product_category (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL
+);
+
+-- brand table
+CREATE TABLE brand (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL
+);
+
+-- color table
+CREATE TABLE color (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL
+);
